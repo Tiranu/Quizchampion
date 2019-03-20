ec3f91d0877f0e8d2f0a4719b7f434f63336df69
fricke <fricke@denniss-mbp-150.fritz.box>
2019-03-20 23:03:03 +0100

    +erste Frage in InformatikFuellen hinzugefügt

--- Quizchampion/src/Fragenkatalog.java
+++ Quizchampion/src/Fragenkatalog.java
@@ -15,6 +15,7 @@ public class Fragenkatalog {
 		{
 			fragenliste.put(kategorien.get(i), new ArrayList<Frage>());
 		}
+		informatikFuellen(kategorien.indexOf("Informatik")+1);
 		System.out.println(fragenliste);
 	}
 	
@@ -24,7 +25,20 @@ public class Fragenkatalog {
 		kategorien.add("Informatik");
 		kategorien.add("Universität");
 		kategorien.add("Games");
-		kategorien.add("Biologie");
-		
+		kategorien.add("Biologie");	
+	}
+	
+	private void informatikFuellen(int kategorieZahl)
+	{
+		int i = 1;
+		String kategorie = "Informatik";
+		if (fragenliste.containsKey(kategorie))
+		{
+		ArrayList<Frage> liste = fragenliste.get(kategorie);
+		liste.add(new Frage(i,kategorieZahl,"Wieviele Bit hat ein Byte", "2", "4", "8", "1024", "c"));
+		i++;
+		}else {
+			System.out.println("Kategorie nicht enthalten");
+		}
 	}
 }
